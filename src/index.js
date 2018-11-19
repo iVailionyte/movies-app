0ad9006525db1c0b798678dde8c40bade329d907
iVailionyte <indre.vailionyte@gmail.com>
2018-11-19 14:11:16 +0100

    feat: create App component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Movies from './components/Movies';
+import App from './components/App';
 
 
-ReactDOM.render(<Movies />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
