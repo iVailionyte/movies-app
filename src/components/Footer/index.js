70538660da02734b2b9b9efcd4bb8cf98231c003
iVailionyte <indre.vailionyte@gmail.com>
2018-11-22 15:54:18 +0100

    feat: add CSS files for Header and Footer components

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import './style.css';
 
 const Footer = (props) => (
-  <footer>
+  <footer className="footer">
     {props.children}
   </footer>
 )
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import './style.css';
 
 const Header = (props) => (
-  <header>
+  <header className="header">
     {props.children}
   </header>
 )
