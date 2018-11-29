2819f17b522ad0a0400ac938fbd21b73fecd2fe5
iVailionyte <indre.vailionyte@gmail.com>
2018-11-29 11:21:04 +0100

    Feat: Create Search page

---
+++ src/pages/Search/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SearchPage = () => (
+  <div>
+    <form>
+      <div>
+        <input type="search" />
+      </div>
+    </form>
+  </div>
+)
+
+export default SearchPage;
