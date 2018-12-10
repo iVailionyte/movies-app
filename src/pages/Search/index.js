1e2da743215c555bbab3c6e211f68c53dc2bac7c
iVailionyte <indre.vailionyte@gmail.com>
2018-12-10 16:50:10 +0100

    Feat: Convert SearchPage to a stateful component

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchPage = () => (
-  <div>
-    <form>
+class SearchPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.submitHandler = this.submitHandler.bind(this);
+    this.inputHandler = this.inputHandler.bind(this);
+  }
+
+  inputHandler(event) {
+    this.setState({value: event.target.value});
+  }
+
+  submitHandler(event) {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
       <div>
-        <input type="search" />
+        <form onSubmit={this.submitHandler}>
+          <div>
+            <input type="search" value={this.state.value} onInput={this.inputHandler} />
+          </div>
+        </form>
       </div>
-    </form>
-  </div>
-)
+    );
+  }
+}
 
 export default SearchPage;
